4f7bf79c8b6f16b75d7d1613de9f224ec0af6c48
satyajit5007 <bhosalesatyajit5007@gmail.com>
2025-03-17 00:13:38 +0530

    test

--- src/Main.java
+++ src/Main.java
@@ -253,5 +253,8 @@ public class Main {
         Car car2 = new Car("C002", "Honda", "Accord", 70.0);
         Car car3 = new Car("C003", "Mahindra", "Thar", 150.0);
 
+        rentalSystem.addCar(car1);
+        rentalSystem.addCar(car2);
+        rentalSystem.addCar(car3);
     }
 }
