cf6cab4a2e00510b6df3535a49bf08e96e2e514a
satyajit5007 <bhosalesatyajit5007@gmail.com>
2025-03-16 23:55:14 +0530

    customer

--- src/Main.java
+++ src/Main.java
@@ -45,6 +45,23 @@ class Car {
     }
 }
 
+class customer{
+    private String customerId;
+    private String name;
+
+    public customer(String customerId, String name) {
+        this.customerId = customerId;
+        this.name = name;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
 
 
 public class Main {
